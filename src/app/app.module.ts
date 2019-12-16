3cb5c7c823de09ca8a5bbb9483af523489cc187f
ekater1na <ekaterina_l@tut.by>
2019-12-16 19:09:39 +0300

    feat: add form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component'
+import { CardComponent } from './card/card.component';
+import { FormComponent } from './form/form.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
+    FormComponent,
   ],
   imports: [
     BrowserModule
