0ee63ce2a5c558e0b45878f8720bb214e23a4afb
ekater1na <ekaterina_l@tut.by>
2019-12-18 11:52:47 +0300

    commit: add logo and button

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,8 +1,28 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
+  interpolation: ['{{', '}}']
 })
-export class CardComponent {}
+export class CardComponent implements OnInit{
+
+  title: string = 'My card title'
+  text: string = 'My text. La-la-la'
+
+  disabled = false
+  
+  imgUrl: string = 'https://icon-library.net/images/github-logo-icon-30_31284.png'
+
+  ngOnInit() {
+    setTimeout(()=>{
+      this.imgUrl = 'https://www.iconsdb.com/icons/preview/black/skype-xxl.png'
+      this.disabled = true
+    }, 3000)
+    
+  }
+  getInfo() {
+    return 'This is info'
+  }
+}
