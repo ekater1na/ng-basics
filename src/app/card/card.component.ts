05db1884cb117e215cf5bf0b2fbacab523537644
ekater1na <ekaterina_l@tut.by>
2019-12-18 13:46:39 +0300

    feat: try to use binding

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -19,4 +19,9 @@ export class CardComponent implements OnInit{
   changeTitle() {
     this.title = 'Title has been changed'
   }
+
+  inputHandler(event: any) {
+    const value = event.target.value
+    this.title = value
+  }
 }
