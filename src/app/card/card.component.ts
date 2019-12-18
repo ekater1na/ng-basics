ca815e1d8bea79f22c7b8f5742ceb9000f79e9f1
ekater1na <ekaterina_l@tut.by>
2019-12-18 13:26:40 +0300

    feat: add buttons

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -4,25 +4,19 @@ import {Component, OnInit} from '@angular/core'
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
-  interpolation: ['{{', '}}']
 })
 export class CardComponent implements OnInit{
 
   title: string = 'My card title'
   text: string = 'My text. La-la-la'
 
-  disabled = false
-  
-  imgUrl: string = 'https://icon-library.net/images/github-logo-icon-30_31284.png'
-
   ngOnInit() {
-    setTimeout(()=>{
-      this.imgUrl = 'https://www.iconsdb.com/icons/preview/black/skype-xxl.png'
-      this.disabled = true
-    }, 3000)
-    
   }
-  getInfo() {
+    getInfo() {
     return 'This is info'
+  }
+
+  changeTitle() {
+    this.title = 'Title has been changed'
   }
 }
